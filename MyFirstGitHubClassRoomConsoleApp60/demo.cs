661b8b7752065e9f967a8b1120d3a7f712572cde
Cem Turan <cemx0121@edu.zealand.dk>
2022-09-05 12:39:08 +0200

    Tilføjet properties.

---
+++ MyFirstGitHubClassRoomConsoleApp60/demo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstGitHubClassRoomConsoleApp60
+{
+    internal class demo
+    {
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Description { get; set; }
+
+
+    }
+}
